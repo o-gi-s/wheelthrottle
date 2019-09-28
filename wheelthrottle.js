ea2010ff419c63c4710540a2eba4249565ee998a
saitoh255 <dr.bob243247@gmail.com>
2019-09-29 01:14:03 +0900

    Tranpale to es5

--- wheelthrottle.js
+++ wheelthrottle.js
@@ -1,41 +1,41 @@
-let tmpArray = [];
-let getTime = new Date().getTime();
-let scrollPossible = true;
+var tmpArray = [];
+var getTime = new Date().getTime();
+var scrollPossible = true;
 
-const calcBalance = (elements, number) => {
-  const LAST_ELEMENTS = elements.slice(Math.max(elements.length - number, 1));
-  let sum = 0;
-  for(let i = 0; i < LAST_ELEMENTS.length; i++){
+var calcBalance = function calcBalance(elements, number) {
+  var LAST_ELEMENTS = elements.slice(Math.max(elements.length - number, 1));
+  var sum = 0;
+  for (var i = 0; i < LAST_ELEMENTS.length; i++) {
     sum = sum + LAST_ELEMENTS[i];
   }
-  return Math.ceil( sum / number );
-}
+  return Math.ceil(sum / number);
+};
 
-const scrollPage = (e) => {
+var scrollPage = function scrollPage(e) {
   scrollPossible = false;
-  setTimeout(() => {
+  setTimeout(function () {
     scrollPossible = true;
   }, 700);
-}
+};
 
-document.addEventListener('wheel', function(e) {
+document.addEventListener('wheel', function (e) {
   e = e || window.event;
-  let value = e.wheelDelta || -e.deltaY || -e.detail;
+  var value = e.wheelDelta || -e.deltaY || -e.detail;
   value = Math.abs(value);
-  if(tmpArray.length > 149){
+  if (tmpArray.length > 149) {
     tmpArray.shift();
   }
   tmpArray.push(Math.abs(value));
-  const TIME_CURRENT = new Date().getTime();
-  const TIME_DIFFERENCE = TIME_CURRENT - getTime;
+  var TIME_CURRENT = new Date().getTime();
+  var TIME_DIFFERENCE = TIME_CURRENT - getTime;
   getTime = TIME_CURRENT;
-  if(TIME_DIFFERENCE > 200){
+  if (TIME_DIFFERENCE > 200) {
     tmpArray = [];
   }
-  const BALANCE_END = calcBalance(tmpArray, 10);
-  const BALANCE_MIDDLE = calcBalance(tmpArray, 70);
-  if(scrollPossible){
-    if(BALANCE_END >= BALANCE_MIDDLE){
+  var BALANCE_END = calcBalance(tmpArray, 10);
+  var BALANCE_MIDDLE = calcBalance(tmpArray, 70);
+  if (scrollPossible) {
+    if (BALANCE_END >= BALANCE_MIDDLE) {
       scrollPage();
       var event = document.createEvent('MouseEvents');
       event.initEvent('wheelthrottle', true, false);
